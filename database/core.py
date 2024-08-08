5c78e6dc9ea6d88002f5a52507a0237965f86fb0
AenEnlil <morphinegermanika@gmail.com>
2024-08-08 14:44:37 +0300

    database: tables moved to separate module, added new exception for database layer

--- database/core.py
+++ database/core.py
@@ -1,40 +1,12 @@
-import sqlite3
-from enum import Enum
-from typing import Literal, List
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from database.tables import Base
 
-from sqlalchemy import create_engine, text, String, CheckConstraint, DateTime, BigInteger, PickleType
-from sqlalchemy.orm import Session, declarative_base, mapped_column, Mapped, validates
-
-Base = declarative_base()
 engine = create_engine("sqlite:///database/drops.db") # prod
 
 # engine = create_engine("sqlite:///drops.db")
 
 print('exec core')
-
-status_validation = "status == 'open' or status == 'closed'"
-
-
-class Campaigns(Base):
-
-    __tablename__ = "campaigns"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    game: Mapped[str] = mapped_column(String(60))
-    company: Mapped[str] = mapped_column(String(60))
-    campaign_name: Mapped[str] = mapped_column(String(60))
-    status: Mapped[str] = mapped_column(String(10), CheckConstraint(status_validation))
-    start_date: Mapped[str] = mapped_column(DateTime)
-    end_date: Mapped[str] = mapped_column(DateTime)
-
-
-class Users(Base):
-
-    __tablename__ = "users"
-
-    user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
-    subscribed_games: Mapped[List] = mapped_column(PickleType(), default=[])
-
 
 Base.metadata.create_all(engine)
 
---
+++ database/tables.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from sqlalchemy import String, CheckConstraint, DateTime, BigInteger, PickleType
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+
+Base = declarative_base()
+
+status_validation = "status == 'open' or status == 'closed'"
+
+
+class Campaigns(Base):
+
+    __tablename__ = "campaigns"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    game: Mapped[str] = mapped_column(String(60))
+    company: Mapped[str] = mapped_column(String(60))
+    campaign_name: Mapped[str] = mapped_column(String(60))
+    status: Mapped[str] = mapped_column(String(10), CheckConstraint(status_validation))
+    start_date: Mapped[str] = mapped_column(DateTime)
+    end_date: Mapped[str] = mapped_column(DateTime)
+
+
+class Users(Base):
+
+    __tablename__ = "users"
+
+    user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
+    subscribed_games: Mapped[List] = mapped_column(PickleType(), default=[])
